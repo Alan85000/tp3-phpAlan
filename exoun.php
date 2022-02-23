912267ff626cbbc7113d134b4a9b1e9f21ec66e1
Alan85000 <mathealan@gmail.com>
2022-02-23 11:29:39 +0100

    Ajoute exercice 1

---
+++ exoun.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1</title>
+</head>
+<body>
+    <?php
+    $phrase= "Le fromage est mangé par la souris qui elle-même se fait manger par le chat, pas de bol !";
+    $phraseModif = ucwords($phrase);
+    echo $phraseModif;
+    ?>
+</body>
+</html>
